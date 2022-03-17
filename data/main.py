be58fdfb5ccdc4872166e9cb692a8c1e61830d6c
B1acKy1in <2421165044@qq.com>
2022-03-17 23:16:27 +0800

    feat(data): add all wh price

--- data/main.py
+++ data/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fund import get_fund_base_info
+from wh import get_wh_daily_price
 
 app = FastAPI()
 
@@ -7,3 +8,6 @@ app = FastAPI()
 async def update_fund_base_info():
     return get_fund_base_info()
 
+@app.get('/api/get_wh_price')
+async def get_wh_price():
+    return get_wh_daily_price()
